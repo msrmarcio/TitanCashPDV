d55645dbeee6090beccf2e177031564684acb3f7
msrmarcio <msrmarcio@hotmail.com>
2024-11-28 23:31:09 -0300

    :wrench: chore: Adicionado comando repository

---
+++ TitanCashPDV/TitanCashPDV.Infrastructure/Repositories/ComandaRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TitanCashPDV.Domain.Entities;
+
+namespace TitanCashPDV.Infrastructure.Repositories
+{
+    public class ComandaRepository : IComandaRepository
+    {
+        private readonly DatabaseContext _context;
+
+        public ComandaRepository(DatabaseContext context)
+        {
+            _context = context;
+        }
+
+        public void Save(Comanda comanda)
+        {
+            // Código para persistir a comanda no banco de dados.
+        }
+    }
+}
